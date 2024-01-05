394271da44be1bee9ea3f4b875e3d1e35ec2c221
lumijoe <spacedesign63@gmail.com>
2024-01-05 20:14:43 +0900

    test btn

--- js/amount.js
+++ js/amount.js
@@ -1,6 +1,19 @@
 const header = new Vue({
     el: '#header',
     data: {
-      apptitle: 'CashNyan'
+      apptitle: '支出入力'
+    }
+  });
+
+const inputON = new Vue({
+    el: '#inputON',
+    methods: {
+        changeTitle: function() {
+            if (header.apptitle === '支出入力') {
+                header.apptitle = '一覧リスト';
+            } else {
+                header.apptitle = '支出入力';
+            }
+        }
     }
   });
